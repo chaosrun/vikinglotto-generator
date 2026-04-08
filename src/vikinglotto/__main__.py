3f719c9cd7e5675cedb2ce4aad6f1c47c9eab832
Zhaosi Qu <git@chaos.run>
2026-04-08 22:08:49 +0300

    chore: bump version to 0.0.7 and drop support for Python < 3.9

--- src/vikinglotto/__main__.py
+++ src/vikinglotto/__main__.py
@@ -3,143 +3,79 @@ from argparse import ArgumentParser, BooleanOptionalAction
 from secrets import SystemRandom
 from shutil import which
 from subprocess import run
-from typing import List
 
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s %(message)s",
-    level=logging.WARNING,
+    level=logging.INFO,
     filename="vikinglotto.log",
     filemode="a"
 )
 logger = logging.getLogger(__name__)
 
 
-def generate_main_numbers(generator: SystemRandom, euro: bool) -> List[int]:
-    # Generate main numbers
-    result = []
-
-    try:
-        max_number = 48 if not euro else 50
-        select_numbers = 6 if not euro else 5
-        result = generator.sample(list(range(1, max_number + 1)), select_numbers)
-        result.sort()
-    except Exception as e:
-        logger.warning(f"Generate main numbers error: {e}", exc_info=True)
-
+def generate_main_numbers(generator: SystemRandom, euro: bool) -> list[int]:
+    max_number = 48 if not euro else 50
+    select_numbers = 6 if not euro else 5
+    result = generator.sample(list(range(1, max_number + 1)), select_numbers)
+    result.sort()
     return result
 
 
-def generate_bonus_numbers(generator: SystemRandom, euro: bool) -> List[int]:
-    # Generate bonus numbers
-    result = []
-
-    try:
-        result = [generator.randint(1, 5)] if not euro else generator.sample(list(range(1, 12 + 1)), 2)
-        result.sort()
-    except Exception as e:
-        logger.warning(f"Generate bonus numbers error: {e}", exc_info=True)
-
+def generate_bonus_numbers(generator: SystemRandom, euro: bool) -> list[int]:
+    result = [generator.randint(1, 5)] if not euro else generator.sample(list(range(1, 12 + 1)), 2)
+    result.sort()
     return result
 
 
-def generate_text(main_numbers: List[int], bonus_numbers: List[int]) -> str:
-    # Generate predict text string
-    result = ""
-
-    try:
-        result = f"Main numbers:\t" + " ".join(f"({n:02})" for n in main_numbers) + "\n\n"
-        bonus_prefix = "Bonus number:\t" if len(bonus_numbers) == 1 else "Bonus numbers:\t"
-        bonus_text = " ".join(f"({n:02})" for n in bonus_numbers)
-        result += bonus_prefix + bonus_text
-    except Exception as e:
-        logger.warning(f"Generate text error: {e}", exc_info=True)
-
+def generate_text(main_numbers: list[int], bonus_numbers: list[int]) -> str:
+    result = "Main numbers:\t" + " ".join(f"({n:02})" for n in main_numbers) + "\n\n"
+    bonus_prefix = "Bonus number:\t" if len(bonus_numbers) == 1 else "Bonus numbers:\t"
+    bonus_text = " ".join(f"({n:02})" for n in bonus_numbers)
+    result += bonus_prefix + bonus_text
     return result
 
 
-def print_log(text: str) -> bool:
-    # Print numbers to log file
-    result = False 
-
-    try:
-        text = text.replace("\n\n", "\n")
-        logger.warning(f"------------------------\n\n"
-                       f"{text}\n")
-        result = True
-    except Exception as e:
-        logger.warning(f"Print log error: {e}", exc_info=True)
-    
-    return result
+def print_log(text: str) -> None:
+    text = text.replace("\n\n", "\n")
+    logger.info(f"------------------------\n\n"
+                f"{text}\n")
 
 
 def show_cowsay(text: str) -> bool:
-    # Print cowsay
-    result = False
+    if which("cowsay") is None:
+        print("\nPlease install cowsay. Otherwise, please use --plain flag\n")
+        return False
 
-    try:
-        if which("cowsay") is None:
-            print("\nPlease install cowsay. Otherwise, please use --plain flag\n")
-            return False
-
-        print()
-        run(f"cowsay -W 100 '\n\n{text}\n\n\b'", shell=True)
-        print()
-        print_log(text)
-        result = True
-    except Exception as e:
-        logger.warning(f"Show cowsay error: {e}", exc_info=True)
-
-    return result
+    print()
+    run(["cowsay", "-W", "100", f"\n\n{text}\n\n"])
+    print()
+    print_log(text)
+    return True
 
 
 def show_plain(text: str) -> bool:
-    # Print directly
-    result = False
-
-    try:
-        text = text.replace("\n\n", "\n")
-        print(f"\n{text}\n")
-        print_log(text)
-        result = True
-    except Exception as e:
-        logger.warning(f"Show plain error: {e}", exc_info=True)
-
-    return result
+    text = text.replace("\n\n", "\n")
+    print(f"\n{text}\n")
+    print_log(text)
+    return True
 
 
 def show_text(plain: bool, text: str) -> bool:
-    # Print the final result
-    result = False
-
-    try:
-        result = show_plain(text) if plain else show_cowsay(text)
-    except Exception as e:
-        logger.warning(f"Show text error: {e}", exc_info=True)
-
-    return result
+    return show_plain(text) if plain else show_cowsay(text)
 
 
-def main() -> bool:
-    # Main function
-    result = False
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("--plain", action=BooleanOptionalAction, help="print the result in plain mode")
+    parser.add_argument("--euro", action=BooleanOptionalAction, help="generate numbers for Eurojackpot")
+    args = parser.parse_args()
 
-    try:
-        parser = ArgumentParser()
-        parser.add_argument("--plain", action=BooleanOptionalAction, help="print the result in plain mode")
-        parser.add_argument("--euro", action=BooleanOptionalAction, help="generate numbers for Eurojackpot")
-        args = parser.parse_args()
-
-        secret_generator = SystemRandom()
-        main_numbers = generate_main_numbers(secret_generator, args.euro)
-        bonus_numbers = generate_bonus_numbers(secret_generator, args.euro)
-        text = generate_text(main_numbers, bonus_numbers)
-        show_text(args.plain, text)
-        result = True
-    except Exception as e:
-        logger.warning(f"Main error: {e}", exc_info=True)
-
-    return result
+    secret_generator = SystemRandom()
+    main_numbers = generate_main_numbers(secret_generator, args.euro)
+    bonus_numbers = generate_bonus_numbers(secret_generator, args.euro)
+    text = generate_text(main_numbers, bonus_numbers)
+    show_text(args.plain, text)
 
 
 if __name__ == "__main__":
